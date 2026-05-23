4e01bdee9a67e7f3791fa65acf14cc8b770f3887
alysson778 <alyssonmizareis@gmail.com>
2026-05-23 17:19:34 -0300

    função de novo projeto em andamento

--- forum/views.py
+++ forum/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Projeto
+from .forms import ForumForm
 
 def lista_projetos(request):
     projetos = Projeto.objects.all()
     return render(request, 'forum/lista.html',{'projetos': projetos})
 
 
+def novo_projeto(request):
+    
+    if request.method == 'POST':
+        form = ForumForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect('forum:lista_projetos')
+    else:
+        template_name = 'form_produto.html'
+        context = {
+            'form': ForumForm(),
+        }
+        return render(request, template_name, context)
 
 
