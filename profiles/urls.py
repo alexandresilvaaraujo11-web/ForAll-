f7e5b72eca4c5e7597f47b29ee54f9bf9abac5b4
Misael <misaelrh@proton.me>
2026-05-23 18:56:55 -0300

    feat: login view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import reg #, new_entrada, update_entrada, delete_entrada 
+from .views import register, login #, new_entrada, update_entrada, delete_entrada 
 app_name = 'profiles' 
 
 urlpatterns = [ 
-    path('reg', reg, name='reg'),
+    path('register', register, name='register'),
+    path('login', login, name='login'),
     #path('list_entrada/', list_entrada, name='list_entrada'), 
     #path('new_entrada/', new_entrada, name='new_entrada'), 
     #path('update_entrada/<int:pk>/', update_entrada, name='update_entrada'), 
--- profiles/views.py
+++ profiles/views.py
@@ -2,7 +2,11 @@ from django.shortcuts import render, redirect
 from .models import Profile
 from .forms import ProfileCreationForm
 
-def reg(request):
+#   imports pra login
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login as auth_login
+
+def register(request):
     if request.method == 'POST':
         form = ProfileCreationForm(request.POST)
         if form.is_valid():
@@ -11,8 +15,36 @@ def reg(request):
         
     else:
         form = ProfileCreationForm()
-    template_name = 'reg.html'
+    template_name = 'register.html'
     context = {
-        'form':ProfileCreationForm()
+        'form':form
     }
     return render(request, template_name, context)
+
+def login(request):
+    if request.method == 'POST':
+        # authentication form recebe o request e os dados separados
+
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            # Pegamos o usuário e senha digitado
+            usuario = form.cleaned_data.get('username')
+            senha = form.cleaned_data.get()
+
+            # authenticate() vai no banco de dados e checa se tão correto
+            user = authenticate(username=usuario, passwrod=senha)
+
+            if user is not None:
+                #auth login é oq loga
+                auth_login(request, user)
+
+                # para onde ele vai depois de logar?
+                return redirect('profile:reg')
+        else:
+            form = AuthenticationForm()
+        context = {
+            'form':form
+        }
+        return render(request, 'login.html', context)
+
