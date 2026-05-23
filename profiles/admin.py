2cadca9a9cafc944db46047a0c76e2f039fd9036
Misael <misaelrh@proton.me>
2026-05-23 16:28:45 -0300

    chore: estrutura models e admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Profile
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extras', {
+            'fields': ('curso', 'bio')
+        }),
+    )
+
+admin.site.register(Profile, CustomUserAdmin)
--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser 
 
-# Create your models here.
+class Cursos(models.Model):
+    curso = models.CharField('Curso', max_lenght=200)
+
+    def __str__(self):
+        return self.curso
+
+class Profile(AbstractUser):
+    curso = models.ForeignKey(Cursos, on_delete=models.CASCADE, verbose_name='Curso')
+    bio = models.TextField('Bio', max_length=500, blank=True, default="")
+
+    def __str__(self):
+        return self.nome
+
