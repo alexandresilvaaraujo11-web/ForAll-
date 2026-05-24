2596125718afae419f1aa9fd6b0de25331f230f9
Misael <misaelrh@proton.me>
2026-05-24 01:01:10 -0300

    style: add css geral bonito no site

--- forum/views.py
+++ forum/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render, redirect
+from django.db.models import Q
 from .models import Projeto
 from .forms import ForumForm
 
 
 def lista_projetos(request):
-    projetos = Projeto.objects.all()
-    return render(request, 'forum/lista.html',{'projetos': projetos})
 
+    projetos = Projeto.objects.all()
+
+    termo = request.GET.get('q', '').strip()
+    curso_selecionado = request.GET.get('curso', '').strip()
+
+    # pesquisa
+    if termo:
+        projetos = projetos.filter(
+            titulo__icontains=termo
+        ) | projetos.filter(
+            descricao__icontains=termo
+        ) | projetos.filter(
+            criador_nome__icontains=termo
+        )
+
+    # filtro
+    if curso_selecionado:
+        projetos = projetos.filter(
+            curso=curso_selecionado
+        )
+
+    cursos = Projeto.objects.values_list(
+        'curso',
+        flat=True
+    ).distinct()
+
+    return render(request, 'forum/lista.html', {
+        'projetos': projetos,
+        'cursos': cursos,
+        'termo': termo,
+        'curso_selecionado': curso_selecionado,
+    })
 
 def novo_projeto(request):
     
