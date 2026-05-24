90a67e3c0fddf47786e355fe55f56ac6e3b1047e
alysson778 <alyssonmizareis@gmail.com>
2026-05-24 12:43:28 -0300

    models e forms.py arrumados

--- forum/forms.py
+++ forum/forms.py
@@ -1,7 +1,11 @@
-from django.forms import ModelForm
+from django.forms import ModelForm,CheckboxSelectMultiple
 from .models import Projeto
 
 class ForumForm (ModelForm):
     class Meta:
         model = Projeto
         fields = ['curso','titulo','descricao','participantes_qtd']
+
+        widgets = {
+            'cursos':CheckboxSelectMultiple(),
+        }
